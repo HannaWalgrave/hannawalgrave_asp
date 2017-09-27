7d1217819f65417d966c43d026fb00304d74edae
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2017-09-27 16:10:53 +0200

    initial commit

---
+++ HannaWalgrave_aspcore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HannaWalgrave_aspcore.Models;
+
+namespace HannaWalgrave_aspcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            var model = new AboutModel();
+            model.Name = "Hanna";
+            model.DaysUntillBirthday = (new DateTime(2018,09,20) - DateTime.Now).TotalDays; 
+            return View(model);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HannaWalgrave_aspcore/Models/AboutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HannaWalgrave_aspcore.Models
+{
+    public class AboutModel
+    {
+        public string Name { get; set; }
+        public double DaysUntillBirthday { get; set; }
+        public DateTime Now => DateTime.Now;
+    }
+}
